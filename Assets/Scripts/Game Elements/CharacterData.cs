540888a7ccd31c5be171491136a2e3774886b96c
Lorenzo Ortiz <jortiz3@rrc.ca>
2023-10-31 10:30:03 -0500

    - Added CharacterInstance class.

--- Assets/Scripts/Game Elements/CharacterData.cs
+++ Assets/Scripts/Game Elements/CharacterData.cs
@@ -2,9 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CharacterData : MonoBehaviour
+[CreateAssetMenu(fileName = "New Card Event", menuName = "Cards/Card Event")]
+public class CharacterData : ScriptableObject
 {
     [SerializeField] private string _name;
+    // Probably art assets here too.
 
     public string Name => _name;
 }
