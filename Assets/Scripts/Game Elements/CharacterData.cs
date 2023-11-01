6970895aded95a83f3d8fdc07e7d569cf574d2e7
Lorenzo Ortiz <jortiz3@rrc.ca>
2023-11-01 11:18:47 -0500

    - Converted CharacterData into Monobehaviour.

--- Assets/Scripts/Game Elements/CharacterData.cs
+++ Assets/Scripts/Game Elements/CharacterData.cs
@@ -2,11 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "New Character", menuName = "Character")]
-public class CharacterData : ScriptableObject
+public class CharacterData : MonoBehaviour
 {
+    [Header("Character Settings")]
     [SerializeField] private string _name;
+    [SerializeField] private bool _isAlive = true;
     // Probably art assets here too.
 
     public string Name => _name;
+    public bool IsAlive => _isAlive;
+
+    public void SetAliveState(bool isAlive)
+    {
+        _isAlive = isAlive;
+    }
 }
