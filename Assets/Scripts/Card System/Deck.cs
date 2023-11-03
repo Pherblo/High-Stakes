57c85eb1cff258ff77a4a67f35883e0eeb7b94c1
Lorenzo Ortiz <jortiz3@rrc.ca>
2023-11-03 10:13:26 -0500

    - Deck now uses characters and gets card from them.

--- Assets/Scripts/Card System/Deck.cs
+++ Assets/Scripts/Card System/Deck.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.Events;
 
@@ -7,29 +8,45 @@ public class Deck : MonoBehaviour
 {
     public UnityEvent<CardEvent> OnCardSelected;
 
-    // Load all cards
+    [Header("References")]
+    [SerializeField] private CharacterDatabase _database;
 
-    private List<CardEvent> _availableCards = new();
-    private List<CardEvent> _lockedCards = new();
+    // private List<CardEvent> _availableCards = new();
+    // private List<CardEvent> _lockedCards = new();
+    private List<CharacterData> _characters = new();
+    private List<CharacterData> _aliveCharacters = new();
+    private List<CharacterData> _deadCharacters = new();
 
     public List<CardDialogue> selectedDialogues = new();
 
-    public void InitializeDeck()
+    public void Start()
     {
         // Load all the cards.
+        _characters = _database.Characters.ToList();
+        foreach (CharacterData character in _characters)
+        {
+            if (character.IsAlive)
+            {
+                _aliveCharacters.Add(character);
+            }
+            else
+            {
+                _deadCharacters.Add(character);
+            }
+        }
     }
 
     public void PickCard()
     {
-        int randomIndex = Random.Range(0, _availableCards.Count);
-        CardEvent selectedCard = _availableCards[randomIndex];
+        int randomIndex = Random.Range(0, _aliveCharacters.Count);
+        CardEvent selectedCard = _aliveCharacters[randomIndex].GetCard();
         selectedCard.OnDialogueSelected += ProcessCard;
     }
 
     private void ProcessCard(CardEvent card)
     {
         card.OnDialogueSelected = null;
-
+        /*
         // Add cards from _lockedCards into _availableCards if requirements are met.
         foreach(CardEvent lockedCard in _lockedCards)
         {
@@ -41,6 +58,7 @@ public class Deck : MonoBehaviour
         }
 
         _availableCards.Remove(card);
+        */
         //_completedCards.Add(card);
         OnCardSelected?.Invoke(card);
     }
