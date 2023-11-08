2d34719c13e26af80040ddf3c0a409cf67410ebb
agamis28 <agamis@rrc.ca>
2023-11-08 12:01:33 -0600

    Added some debug lines and only returning card to origin when it is within a threshold

--- Assets/Scenes/Card Drag Demo/CardSwipeDrag.cs
+++ Assets/Scenes/Card Drag Demo/CardSwipeDrag.cs
@@ -10,28 +10,45 @@ public class CardSwipeDrag : MonoBehaviour, IDragHandler, IBeginDragHandler, IEn
 	[SerializeField] private float swipeDistance;
 	private float cardSwipeThreshold = 200;
 
-	// When you drag the card. Move its local x position, add the amount moved on x axis.
-	public void OnDrag(PointerEventData eventData)
+	[Header("** Console Debug Logs **")]
+	[SerializeField] private bool debug_CurrentSwipeDistnace = false;
+    [SerializeField] private bool debug_Dragging = false;
+
+
+    // When you drag the card. Move its local x position, add the amount moved on x axis.
+    public void OnDrag(PointerEventData eventData)
 	{
 		transform.localPosition = new Vector2(transform.localPosition.x + eventData.delta.x, transform.localPosition.y);
-	}
 
-	// When you first begin to drag the card, store its inital origin position;
-	public void OnBeginDrag(PointerEventData eventData)
+        // ** Debugs **
+        if (debug_CurrentSwipeDistnace) { Debug.Log("Current Swipe Distance" + Mathf.Abs(transform.localPosition.x - cardOriginPosition.x)); }
+		if (debug_Dragging) { Debug.Log("Is dragging"); }
+    }
+
+    // When you first begin to drag the card, store its inital origin position;
+    public void OnBeginDrag(PointerEventData eventData)
 	{
 		// Take the inital origin position and cache it into cardOriginPosition
 		cardOriginPosition = transform.localPosition;
-		Debug.Log("Starting Drag");
-	}
 
-	// When you release the drag.
-	public void OnEndDrag(PointerEventData eventData)
+        // ** Debugs **
+        if (debug_Dragging) { Debug.Log("Started to drag"); }
+    }
+
+    // When you release the drag.
+    public void OnEndDrag(PointerEventData eventData)
 	{
 		// Take the new position of card and take out the origin position. To find the swipe distance
 		// Make it an absolute number to always be a positive magnitude
 		swipeDistance = Mathf.Abs(transform.localPosition.x - cardOriginPosition.x);
 
-		// If not past a certain threshold.
-		transform.localPosition = cardOriginPosition;
-	}
+		// Only revert the cards position when within the threshold
+		if (swipeDistance < cardSwipeThreshold)
+		{
+            transform.localPosition = cardOriginPosition;
+        }
+
+		// ** Debugs **
+        if (debug_Dragging) { Debug.Log("Finished drag"); }
+    }
 }
