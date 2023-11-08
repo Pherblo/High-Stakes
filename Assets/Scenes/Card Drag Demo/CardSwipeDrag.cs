72367298522bca0a45e4b9138d7989939d76ad0d
agamis28 <agamis@rrc.ca>
2023-11-08 14:47:23 -0600

    Made cards go transparent after swiping past the threshold. If we are using animations. They would play before the card would reappear.

--- Assets/Scenes/Card Drag Demo/CardSwipeDrag.cs
+++ Assets/Scenes/Card Drag Demo/CardSwipeDrag.cs
@@ -1,35 +1,55 @@
-using System.Collections;
-using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
 using UnityEngine.EventSystems;
 
 public class CardSwipeDrag : MonoBehaviour, IDragHandler, IBeginDragHandler, IEndDragHandler
 {
-	private Vector3 cardOriginPosition;
-	[SerializeField] private float swipeDistance;
+	private Vector3 cardOriginPosition = new Vector3(0, 35, 0);
+	private float swipeDistance;
 	private float cardSwipeThreshold = 200;
+	private bool swipeIsRight;
+	private Image thisCardArt;
+
+    [SerializeField] private UnityEvent discardCard; // Play discard animation while card is transparent
 
 	[Header("** Console Debug Logs **")]
 	[SerializeField] private bool debug_CurrentSwipeDistnace = false;
     [SerializeField] private bool debug_Dragging = false;
+	[SerializeField] private bool debug_SwipeDirection = false;
 
+
+    private void Start()
+    {
+        thisCardArt = GetComponent<Image>();
+    }
 
     // When you drag the card. Move its local x position, add the amount moved on x axis.
     public void OnDrag(PointerEventData eventData)
 	{
 		transform.localPosition = new Vector2(transform.localPosition.x + eventData.delta.x, transform.localPosition.y);
 
+		// If the x of moved card is more than the origin of the card then you are swiping right.
+        if (transform.localPosition.x > cardOriginPosition.x)
+        {
+            swipeIsRight = true;
+        }
+		else
+		{
+			swipeIsRight = false;
+		}
+
         // ** Debugs **
         if (debug_CurrentSwipeDistnace) { Debug.Log("Current Swipe Distance" + Mathf.Abs(transform.localPosition.x - cardOriginPosition.x)); }
 		if (debug_Dragging) { Debug.Log("Is dragging"); }
+		if (debug_SwipeDirection) { Debug.Log("Swiping right is: " + swipeIsRight); }
     }
 
     // When you first begin to drag the card, store its inital origin position;
     public void OnBeginDrag(PointerEventData eventData)
 	{
 		// Take the inital origin position and cache it into cardOriginPosition
-		cardOriginPosition = transform.localPosition;
+		// cardOriginPosition = transform.localPosition;
 
         // ** Debugs **
         if (debug_Dragging) { Debug.Log("Started to drag"); }
@@ -47,8 +67,28 @@ public class CardSwipeDrag : MonoBehaviour, IDragHandler, IBeginDragHandler, IEn
 		{
             transform.localPosition = cardOriginPosition;
         }
+		else
+		{
+			// Card Swipe animation
+			// Dont show card anymore
+			// Discard animation
+			// Change Card
+			// Draw new card
+			thisCardArt.color = Color.clear;
+			Invoke("DrawNewCard", 5);
+		}
 
 		// ** Debugs **
         if (debug_Dragging) { Debug.Log("Finished drag"); }
     }
+
+	public void DrawNewCard()
+	{
+		// From card displayer.
+		// From animation
+
+		// For now demo logic
+		transform.localPosition = cardOriginPosition;
+		thisCardArt.color = Color.white;
+	}
 }
