4db02d756f6025723ce803b4f2f5c5a20fb81359
Jose Lorenzo Ortiz <jortiz3@rrc.ca>
2023-11-09 20:36:59 -0600

    - Refactored CardCondition.

--- Assets/Scripts/Card System/CardCondition.cs
+++ Assets/Scripts/Card System/CardCondition.cs
@@ -3,28 +3,73 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum LifeCondition
+{
+    None = 0,
+    Alive = 1,
+    Dead = 2
+}
+
+public enum ChoiceCondition
+{
+    None = 0,
+    ChoiceA = 1,
+    ChoiceB = 2
+}
+
 [Serializable]
 public class CardCondition
 {
-    [SerializeField] private CardEvent _cardCondition;
-    [SerializeField] private SelectedChoice _choiceCondition = 0;
+    [Header("Selected Dialogue Condition")]
+    [SerializeField] private CardEvent _cardReference;
+    [SerializeField] private ChoiceCondition _choiceCondition = ChoiceCondition.None;
+    [Header("Character Life Condition")]
+    [SerializeField] private CharacterData _characterReference;
+    [SerializeField] private LifeCondition _lifeCondition = LifeCondition.None;
 
     public bool CheckCondition(List<CardDialogue> selectedChoices)
     {
         // Get the required choice.
-        CardDialogue choiceCondition;
-        if (_choiceCondition == SelectedChoice.ChoiceA)
+        CardDialogue choiceCondition = null;
+        if (_choiceCondition != ChoiceCondition.None)
         {
-            choiceCondition = _cardCondition.DialogueA;
-        }
-        else
-        {
-            choiceCondition = _cardCondition.DialogueB;
+            if (_choiceCondition == ChoiceCondition.ChoiceA)
+            {
+                choiceCondition = _cardReference.DialogueA;
+            }
+            else if (_choiceCondition == ChoiceCondition.ChoiceB)
+            {
+                choiceCondition = _cardReference.DialogueB;
+            }
+
+            // Check if the required choice was previously chosen. If not, return false.
+            if (selectedChoices.Exists((x) => x == choiceCondition))
+            {
+                return false;
+            }
         }
 
-        // Check if the required choice was previously chosen.
-        bool hasSelectedChoice = selectedChoices.Exists((x) => x == choiceCondition);
-        if (hasSelectedChoice) return true;
+        // Evaluate character life prerequisites. TODO: Reference a list that tracks characters' life states.
+        /*
+        if (_lifeCondition != LifeCondition.None)
+        {
+            if (_choiceCondition == ChoiceCondition.ChoiceA)
+            {
+                choiceCondition = _cardReference.DialogueA;
+            }
+            else if (_choiceCondition == ChoiceCondition.ChoiceB)
+            {
+                choiceCondition = _cardReference.DialogueB;
+            }
+
+            // Check if the required choice was previously chosen. If not, return false.
+            if (selectedChoices.Exists((x) => x == choiceCondition))
+            {
+                return false;
+            }
+        }
+        */
+
         return false;
     }
 }
