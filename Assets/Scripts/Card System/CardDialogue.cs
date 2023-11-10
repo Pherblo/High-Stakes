4de74be469f9cc601a352dbd31e3327c7afd7a25
Jose Lorenzo Ortiz <jortiz3@rrc.ca>
2023-11-10 15:35:31 -0600

    - Refactored CardDialogue variables to follow code conventions.

--- Assets/Scripts/Card System/CardDialogue.cs
+++ Assets/Scripts/Card System/CardDialogue.cs
@@ -8,12 +8,12 @@ using UnityEngine.UIElements;
 public class CardDialogue
 {
     [Header("Dialogue Settings")]
-    [SerializeField] private bool dialogueIsActive = true;
-    [SerializeField] private string dialogueOption;
+    [SerializeField] private bool _dialogueIsActive = true;
+    [SerializeField] private string _dialogueText;
     // Card Effects pertain to the effects of the cards once this corresponding dialogue is selected.
     [Header("Card Results Settings")]
     [SerializeField] private CharacterData[] _charactersToBeDead = new CharacterData[0];
 
     public CharacterData[] CharactersToBeDead => _charactersToBeDead;
-    public string Dialogue => dialogueOption;
+    public string DialogueText => _dialogueText;
 }
