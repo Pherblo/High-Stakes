b301ab58ac1f7554138078b13a54917a465fe2ca
Jose Lorenzo Ortiz <jortiz3@rrc.ca>
2023-11-10 19:16:53 -0600

    - Edited text generation speed of Typewriter classes in demo scene. - Slight refactoring on CardDisplay for code clarity.

--- Assets/Dev_Alex/CardDisplay.cs
+++ Assets/Dev_Alex/CardDisplay.cs
@@ -8,18 +8,21 @@ public class CardDisplay : MonoBehaviour
     [Header("Card UI References")]
     [SerializeField] private SpriteRenderer _characterArtRenderer;
     [SerializeField] private TextMeshProUGUI _cardName;
-    [SerializeField] private TextMeshProUGUI _cardDescription;
-    [SerializeField] private TextMeshProUGUI _choiceAText;
-    [SerializeField] private TextMeshProUGUI _choiceBText;
+    [SerializeField] private Typewriter _cardDescription;
+    [SerializeField] private Typewriter _choiceAText;
+    [SerializeField] private Typewriter _choiceBText;
 
     // Called by events.
     public void UpdateCardDisplay(CardEvent cardToDisplay)
     {
         // Display texts.
         _cardName.text = cardToDisplay.AssociatedCharacter.Name;
-        _cardDescription.text = cardToDisplay.Description;
-        _choiceAText.text = cardToDisplay.DialogueA.DialogueText;
-        _choiceBText.text = cardToDisplay.DialogueB.DialogueText;
+        //_cardDescription.text = cardToDisplay.Description;
+        //_choiceAText.text = cardToDisplay.DialogueA.DialogueText;
+        //_choiceBText.text = cardToDisplay.DialogueB.DialogueText;
+        _cardDescription.RunDialogue(cardToDisplay.Description);
+        _choiceAText.RunDialogue(cardToDisplay.DialogueA.DialogueText);
+        _choiceBText.RunDialogue(cardToDisplay.DialogueB.DialogueText);
         _characterArtRenderer.sprite = cardToDisplay.AssociatedCharacter.CharacterArt;
     }
 }
