f24d812ab2c3f3dfcba3d412c93bdc8e1e803a6d
Lorenzo Ortiz <jortiz3@rrc.ca>
2023-11-15 14:21:11 -0600

    - Fixed build resolution issues.

---
+++ Assets/Scripts/CameraResolutionSetter.cs
@@ -0,0 +1,77 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraResolutionSetter : MonoBehaviour
+{
+    private int ScreenSizeX = 0;
+    private int ScreenSizeY = 0;
+
+    private void RescaleCamera()
+    {
+
+        if (Screen.width == ScreenSizeX && Screen.height == ScreenSizeY) return;
+
+        float targetaspect = 16.0f / 9.0f;
+        float windowaspect = (float)Screen.width / (float)Screen.height;
+        float scaleheight = windowaspect / targetaspect;
+        Camera camera = GetComponent<Camera>();
+
+        if (scaleheight < 1.0f)
+        {
+            Rect rect = camera.rect;
+
+            rect.width = 1.0f;
+            rect.height = scaleheight;
+            rect.x = 0;
+            rect.y = (1.0f - scaleheight) / 2.0f;
+
+            camera.rect = rect;
+        }
+        else // add pillarbox
+        {
+            float scalewidth = 1.0f / scaleheight;
+
+            Rect rect = camera.rect;
+
+            rect.width = scalewidth;
+            rect.height = 1.0f;
+            rect.x = (1.0f - scalewidth) / 2.0f;
+            rect.y = 0;
+
+            camera.rect = rect;
+        }
+
+        ScreenSizeX = Screen.width;
+        ScreenSizeY = Screen.height;
+    }
+
+    void OnPreCull()
+    {
+        if (Application.isEditor) return;
+        Rect wp = Camera.main.rect;
+        Rect nr = new Rect(0, 0, 1, 1);
+
+        Camera.main.rect = nr;
+        GL.Clear(true, true, Color.black);
+
+        Camera.main.rect = wp;
+
+    }
+
+    private void Awake()
+    {
+        Screen.SetResolution(1920, 1080, true);
+    }
+
+    void Start()
+    {
+        RescaleCamera();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //RescaleCamera();
+    }
+}
