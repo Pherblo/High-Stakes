dff4e5eb734c0132280e626ec10f9e2b523efdc1
Lorenzo Ortiz <jortiz3@rrc.ca>
2023-11-17 12:32:16 -0600

    - Started preparations for text animations for Typewriter class. - Changed TMP_Text type references in Typewriter to TextMeshProUGUI types.

--- Assets/Scripts/Typewriter.cs
+++ Assets/Scripts/Typewriter.cs
@@ -6,8 +6,8 @@ using UnityEngine.UI;
 
 public class Typewriter : MonoBehaviour
 {
-    [SerializeField] private TMP_Text _dialogueText;
-    [SerializeField] private TMP_Text _textboxFillerText;        // Used to set text box sizes beforehand. This text component is usually invisible/transparent.
+    [SerializeField] private TextMeshProUGUI _dialogueText;
+    [SerializeField] private TextMeshProUGUI _textboxFillerText;        // Used to set text box sizes beforehand. This text component is usually invisible/transparent.
 
     [SerializeField] private float _timePerChar = 0.1f;
 
@@ -21,6 +21,21 @@ public class Typewriter : MonoBehaviour
     }
 
     private IEnumerator TypeAnimation(string text)
+    {
+        _textboxFillerText.text = text;
+        _characters = text.ToCharArray();
+
+        _dialogueText.text = "";
+
+        for (int i = 0; i < _characters.Length; i++)
+        {
+            _dialogueText.text += _characters[i];
+            if (_characters[i] != ' ')
+                yield return new WaitForSeconds(_timePerChar);
+        }
+    }
+
+    private IEnumerator NewTypeAnimation(string text)
     {
         _textboxFillerText.text = text;
         _characters = text.ToCharArray();
