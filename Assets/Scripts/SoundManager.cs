a5d10f3904c7660516ca332a09924306af4e40a2
Alex <abacon2@academic.rrc.ca>
2023-11-19 22:10:03 -0600

    made music and sound effects be controlled by the settings slider, also adjusted the sliders min and max values

--- Assets/Scripts/SoundManager.cs
+++ Assets/Scripts/SoundManager.cs
@@ -4,15 +4,36 @@ using UnityEngine;
 
 public class SoundManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public GameObject settings;
+    public GameObject backgroundMusic;
+    public GameObject soundEffects;
+
+    private float musicVolume;
+    private float effectsVolume;
     void Start()
     {
         
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
-        
+        setVolume();
+    }
+
+    private void setVolume()
+    {
+        musicVolume = settings.GetComponent<OptionsManager>().musicSlider.value;
+        effectsVolume = settings.GetComponent<OptionsManager>().sfxSlider.value;
+
+        if (backgroundMusic.GetComponent<AudioSource>() != null)
+        {
+            backgroundMusic.GetComponent<AudioSource>().volume = musicVolume;
+        }
+
+        if (soundEffects.GetComponent<AudioSource>() != null)
+        {
+            soundEffects.GetComponent<AudioSource>().volume = effectsVolume;
+        }
     }
 }
