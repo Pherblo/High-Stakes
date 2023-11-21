67d93ff7f5f8fee17dd2789f0775b099763b5d30
Jose Lorenzo Ortiz <jortiz3@rrc.ca>
2023-11-21 12:10:29 -0600

    - Added card movement functionality to CardSwipeAnimator.

--- Assets/Scripts/UI Scripts/CardSwipeAnimator.cs
+++ Assets/Scripts/UI Scripts/CardSwipeAnimator.cs
@@ -5,24 +5,68 @@ using UnityEngine.EventSystems;
 
 public class CardSwipeAnimator : MonoBehaviour, IDragHandler, IBeginDragHandler, IEndDragHandler
 {
+    [Header("Scene References")]
+    [SerializeField] private Camera _cardCamera;
+    [Header("Swipe Animation Settings")]
+    [SerializeField] private float _maxXOffset = 5;
+    [SerializeField] private float _maxRotationOffset = 20f;
+    [SerializeField] private Vector3 _normalizedRotationAxis;     // Must be normalized.
+    [Header("Swipe Boundary Settings")]
+    [SerializeField] private float _swipeXBoundsSize = 10f;
+
+    private Vector3 _originalPosition;
+    private Vector3 _cachedDragStartPosition = Vector3.zero;
+
+    private void Awake()
+    {
+        _originalPosition = transform.position;
+    }
+
     public void OnBeginDrag(PointerEventData eventData)
     {
-        print("ON BEGIN DRAG HAPPENED");
+        // Calculate mouse's world position.
+        float distanceFromCamera = (_originalPosition - _cardCamera.transform.position).magnitude;
+        Vector3 pointerPosition = _cardCamera.ScreenToWorldPoint(new Vector3(eventData.position.x, eventData.position.y, distanceFromCamera));
+        _cachedDragStartPosition = pointerPosition;
     }
 
     public void OnDrag(PointerEventData eventData)
     {
         //
         print("ON DRAG HAPPENED");
+        MoveCard(eventData.position);
     }
 
     public void OnEndDrag(PointerEventData eventData)
     {
-        print("ON END DRAG HAPPENED");
+        StartCoroutine(SnapbackCard());
     }
 
     public void TestEvents()
     {
         print("Event fired");
+    }
+
+    private void MoveCard(Vector3 eventDataPosition)
+    {
+        // Calculate mouse's world position.
+        float distanceFromCamera = (_originalPosition - _cardCamera.transform.position).magnitude;
+        Vector3 pointerPosition = _cardCamera.ScreenToWorldPoint(new Vector3(eventDataPosition.x, eventDataPosition.y, distanceFromCamera));
+
+        // Constrain card movement.
+        Vector3 dragDirection = pointerPosition - _cachedDragStartPosition;
+        var newPosition = _originalPosition + Vector3.ClampMagnitude(dragDirection, _maxXOffset);
+        transform.position = newPosition;
+    }
+
+    private void RotateCard()
+    {
+        //
+    }
+
+    private IEnumerator SnapbackCard()
+    {
+        transform.position = _originalPosition;
+        yield return null;
     }
 }
