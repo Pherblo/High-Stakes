e0eb51b67b3397b6d1e4ce00f116ffa39440a0a0
Jose Lorenzo Ortiz <jortiz3@rrc.ca>
2023-11-20 18:53:17 -0600

    - Finished implementation of UIAnimator. Now it doesn't use an animator component.

--- Assets/Scripts/UI Scripts/UIAnimator.cs
+++ Assets/Scripts/UI Scripts/UIAnimator.cs
@@ -1,21 +1,46 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIAnimator : MonoBehaviour
 {
     // This script is attached to UI components, with Image components, whose materials use the custom UI Fade shader.
     // All methods in this script is called via events.
     [Header("Component References")]
-    [SerializeField] private Animator _animator;
+    [SerializeField] private Image _imageComponent;
+    [Header("Fading Animation Settings")]
+    [SerializeField] private float _fadeDuration = 0.5f;
+    [SerializeField] private float _fadeInRotation = 0f;
+    [SerializeField] private float _fadeOutRotation = 180f;
 
     public void StartEnterFade()
     {
-        _animator.SetTrigger("FadeIn");
+        StopAllCoroutines();
+        StartCoroutine(StartFading(1f, 0f, 0f));
     }
 
     public void StartExitFade()
     {
-        _animator.SetTrigger("FadeOut");
+        StopAllCoroutines();
+        StartCoroutine(StartFading(1f, 0f, 180f));
+    }
+
+    private IEnumerator StartFading(float startValue, float endValue, float newRotation)
+    {
+        // Set fade direction.
+        _imageComponent.material.SetFloat("_FadeDirectionRotation", newRotation);
+        // Start fading.
+        float timer = 0;
+        do
+        {
+            float lerpProgress = timer / _fadeDuration;
+            float lerpValue = Mathf.Lerp(startValue, endValue, lerpProgress);
+            _imageComponent.material.SetFloat("_FadeAlphaClip", lerpValue);
+            timer += Time.deltaTime;
+            yield return null;
+        } while (timer < _fadeDuration);
+
+        yield return null;
     }
 }
