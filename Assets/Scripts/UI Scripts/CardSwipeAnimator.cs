c1852b42c4c60d160cb2bcd5a59139e438198d1e
Jose Lorenzo Ortiz <jortiz3@rrc.ca>
2023-11-21 17:55:48 -0600

    - Added exit movement and rotation for card. (Needs a lot of improvement but it works.)

--- Assets/Scripts/UI Scripts/CardSwipeAnimator.cs
+++ Assets/Scripts/UI Scripts/CardSwipeAnimator.cs
@@ -1,3 +1,4 @@
+using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
 using System.Threading;
@@ -9,11 +10,16 @@ public class CardSwipeAnimator : MonoBehaviour, IDragHandler, IBeginDragHandler,
     [Header("Scene References")]
     [SerializeField] private Camera _cardCamera;
     [Header("Swipe Settings")]
-    [SerializeField] private float _maxXOffset = 5;
+    [SerializeField] private float _maxXOffset = 5f;
     [SerializeField] private float _maxRotationOffset = 20f;
     [SerializeField] private Vector3 _normalizedRotationAxis;     // Must be normalized.
     [Header("Snapback Animation Settings")]
     [SerializeField] private float _snapbackDuration = 0.2f;
+    [Header("Exit Animation Settings")]
+    [SerializeField] private float _exitSpeed = 20f;
+    [SerializeField] private float _exitDuration = 2f;
+    [SerializeField] private float _rotationResetDuration = 0.25f;
+    [SerializeField] private float _exitRotationSpeed = 180f;
 
     private Vector3 _originalPosition;
     private Quaternion _originalRotation;
@@ -39,15 +45,15 @@ public class CardSwipeAnimator : MonoBehaviour, IDragHandler, IBeginDragHandler,
 
     public void OnDrag(PointerEventData eventData)
     {
-        //
-        print("ON DRAG HAPPENED");
         MoveCard(eventData.position);
         RotateCard(eventData.position);
     }
 
     public void OnEndDrag(PointerEventData eventData)
     {
-        StartCoroutine(SnapbackCard());
+        //StartCoroutine(SnapbackCard());
+        print("on drag end");
+        StartCoroutine(StartExitAnimation());
     }
 
     public void TestEvents()
@@ -69,6 +75,7 @@ public class CardSwipeAnimator : MonoBehaviour, IDragHandler, IBeginDragHandler,
 
         float lerpValue = GetLerpValue();
         transform.position = Vector3.Lerp(_originalPosition, targetPosition, lerpValue);
+        _cachedNewPosition = transform.position;
     }
 
     private void RotateCard(Vector3 eventDataPosition)
@@ -100,9 +107,57 @@ public class CardSwipeAnimator : MonoBehaviour, IDragHandler, IBeginDragHandler,
             transform.position = Vector3.Lerp(currentPosition, _originalPosition, timer / _snapbackDuration);
             transform.rotation = Quaternion.Lerp(currentRotation, _originalRotation, timer / _snapbackDuration);
 
+            _cachedNewPosition = transform.position;
+
             yield return null;
         } while (timer < _snapbackDuration);
 
+        yield return null;
+    }
+
+    private IEnumerator StartExitAnimation()
+    {
+        float signedDirection = Mathf.Sign(_cachedNewPosition.x - _originalPosition.x);
+        
+        StartCoroutine(StartExitRotationZ());
+
+        float timer = 0f;
+        do
+        {
+            timer += Time.deltaTime;
+
+            // Modify position.
+            transform.position += new Vector3(_exitSpeed * signedDirection * Time.deltaTime, 0f, 0f);
+            _cachedNewPosition = transform.position;
+
+            // Modify rotation.
+            //transform.rotation *= Quaternion.Euler(0f, _exitRotationSpeed * signedDirection * Time.deltaTime, 0f);
+            transform.rotation *= Quaternion.AngleAxis(_exitRotationSpeed * signedDirection * Time.deltaTime, Vector3.up);
+            yield return null;
+        } while (timer < _exitDuration);
+
+        yield return null;
+    }
+
+    private IEnumerator StartExitRotationZ()
+    {
+        float currentZ = transform.rotation.eulerAngles.z;
+        float targetZ = currentZ > 180f ? 360f : 0f;
+        print(currentZ + " " + targetZ);
+        float timer = 0f;
+        do
+        {
+            timer += Time.deltaTime;
+
+            // Modify rotation
+            float newZRotation = Mathf.Lerp(currentZ, targetZ, timer / _rotationResetDuration);
+            print("new z rot " + newZRotation + " timer " + timer);
+            //Quaternion newRotation = Quaternion.AngleAxis(newZRotation, Vector3.forward);
+            //transform.rotation *= newRotation;
+            transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y, newZRotation);
+
+            yield return null;
+        } while (timer < _rotationResetDuration);
         yield return null;
     }
 
