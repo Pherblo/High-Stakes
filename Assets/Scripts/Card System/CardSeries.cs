731b6507ec8e4b327cdb9d82a18eff8fdccd0cf6
Jose Lorenzo Ortiz <jortiz3@rrc.ca>
2023-11-23 16:06:08 -0600

    - Added indexing variable to CardSeries.

--- Assets/Scripts/Card System/CardSeries.cs
+++ Assets/Scripts/Card System/CardSeries.cs
@@ -7,14 +7,26 @@ public class CardSeries : CardBase
     [Header("Cards will be selected in order. This is similar to having a series of cards whose requirements are that the card before them needs to be completed.")]
     [SerializeField] private List<CardEvent> _cardEvents = new();
 
+    private int _seriesIndex = 0;
+
     public override CardEvent GetCard()
     {
-        if (_cardEvents.Count > 1) return _cardEvents[0];
+        // Guard.
+        if (_seriesIndex > _cardEvents.Count) return null;
+
+        if (_cardEvents.Count > 1)
+        {
+            _seriesIndex++;
+            return _cardEvents[_seriesIndex - 1];
+        }
         else return null;
     }
 
     public override bool CheckRequirements()
     {
+        // All cards completed.
+        if (_seriesIndex > _cardEvents.Count) return false;
+
         foreach (CardEvent card in _cardEvents)
         {
             if (card.CheckRequirements()) return true;
