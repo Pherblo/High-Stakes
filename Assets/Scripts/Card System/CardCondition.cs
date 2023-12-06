d8866684123c874a382d4a7a0e03882196da314b
Jose Lorenzo Ortiz <jortiz3@rrc.ca>
2023-12-06 16:49:15 -0600

    - Finalized card logic.

--- Assets/Scripts/Card System/CardCondition.cs
+++ Assets/Scripts/Card System/CardCondition.cs
@@ -17,16 +17,32 @@ public enum ChoiceCondition
     ChoiceB = 2
 }
 
+public enum StatCondition
+{
+    Disabled = 0,
+    Equal = 1,
+    GreaterThan = 2,
+    LessThan = 3,
+}
+
 [Serializable]
 public class CardCondition
 {
     [Header("Selected Dialogue Condition")]
     [SerializeField] private CardEvent _cardReference;
     [SerializeField] private ChoiceCondition _choiceCondition = ChoiceCondition.None;
+
     [Header("Stat Conditions")]
+    [SerializeField] private StatCondition _suspicionCondition = StatCondition.Disabled;
     [SerializeField] private float _suspicionRequirement = 0;
+    [Space]
+    [SerializeField] private StatCondition _soulsCondition = StatCondition.Disabled;
     [SerializeField] private float _soulsRequirement = 0;
+    [Space]
+    [SerializeField] private StatCondition _popularCondition = StatCondition.Disabled;
     [SerializeField] private float _popularityRequirement = 0;
+
+    [Space]
     [Header("Character Life Condition")]
     [SerializeField] private CharacterData _characterReference;
     [SerializeField] private LifeCondition _lifeCondition = LifeCondition.None;
@@ -70,13 +86,33 @@ public class CardCondition
             }
         }
 
-        if (deck.Stats.Suspicion.getValue() < _suspicionRequirement
+        /*if (deck.Stats.Suspicion.getValue() < _suspicionRequirement
                 || deck.Stats.Souls.getValue() < _soulsRequirement
                 || deck.Stats.Popularity.getValue() < _popularityRequirement)
         {
             return false;
-        }
+        }*/
 
+        if (!CheckStat(_suspicionCondition, deck.Stats.Suspicion.getValue(), _suspicionRequirement))
+            return false;
+        else if (!CheckStat(_soulsCondition, deck.Stats.Souls.getValue(), _soulsRequirement))
+            return false;
+        else if (!CheckStat(_popularCondition, deck.Stats.Popularity.getValue(), _popularityRequirement))
+            return false;
+        // If all conditions pass, return true.
         return true;
+    }
+
+    private bool CheckStat(StatCondition condition, float statValue, float comparisonValue)
+    {
+        if (condition == StatCondition.Disabled)
+            return true;
+        else if (condition == StatCondition.Equal && statValue == comparisonValue)
+            return true;
+        else if (condition == StatCondition.GreaterThan && statValue > comparisonValue)
+            return true;
+        else if (condition == StatCondition.LessThan && statValue < comparisonValue)
+            return true;
+        else return false;
     }
 }
